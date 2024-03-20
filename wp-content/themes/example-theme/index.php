6f7dab1ead0f6e68c53df2c0238ab86c7c27bb4b
aihki <laine.nestori.julius@gmail.com>
2024-03-20 19:44:29 +0200

    first push

---
+++ wp-content/themes/example-theme/index.php
@@ -0,0 +1,11 @@
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) :
+        the_post();
+        the_title(before:'<h1>', after:'</h1>');
+        the_content();
+    endwhile;
+else :
+    _e( 'Sorry, no posts matched your criteria.', 'vaikka' );
+endif;
+?>
