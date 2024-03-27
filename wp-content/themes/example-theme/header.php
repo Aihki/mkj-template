398dd0caa9515225092c24d8bfb6d1570e5cf472
aihki <laine.nestori.julius@gmail.com>
2024-03-27 14:09:10 +0200

    theme done?

--- wp-content/themes/example-theme/header.php
+++ wp-content/themes/example-theme/header.php
@@ -15,6 +15,11 @@
             <?php the_custom_logo(); ?>
         </div>
         <div class="header-top-right">
-           <?php wp_nav_menu(['container' => 'nav', 'theme_location' => 'main-menu']); ?>
+            <?php wp_nav_menu(['container' => 'nav', 'theme_location' => 'main-menu']); ?>
         </div>
     </header>
+    <section class="breadcrumbs">
+        <?php if (function_exists('bcn_display')) {
+            bcn_display();
+        } ?>
+    </section>
